9c1e980970f8690199f4fb82d33ef9d1e503afe0
Trey Dockendorf <tdockendorf@osc.edu>
2017-03-06 09:53:25 -0500

    Update to unit tests and acceptance tests

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -42,4 +42,40 @@ describe 'fail2ban class:' do
       it { should be_running }
     end
   end
+
+  context 'ensure => absent' do
+    it 'should run successfully' do
+      pp =<<-EOS
+      class { 'fail2ban': ensure => 'absent' }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    if fact('osfamily') == 'RedHat'
+      if fact('operatingsystemmajrelease') == '7'
+        package_name = 'fail2ban-server'
+      else
+        package_name = 'fail2ban'
+      end
+    end
+
+    describe package(package_name) do
+      it { should_not be_installed }
+    end
+
+    describe file('/etc/fail2ban/fail2ban.local') do
+      it { should_not be_file }
+    end
+
+    describe file('/etc/fail2ban/jail.local') do
+      it { should_not be_file }
+    end
+
+    describe service('fail2ban') do
+      it { should_not be_enabled }
+      it { should_not be_running }
+    end
+  end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,26 +1,38 @@
 require 'beaker-rspec'
-require 'beaker_spec_helper'
-include BeakerSpecHelper
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
 dir = File.expand_path(File.dirname(__FILE__))
 Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
 
-install_puppet_on(hosts, {:version => '3.8.3'})
+run_puppet_install_helper
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  module_name = module_root.split('-').last
-
   # Readable test descriptions
   c.formatter = :documentation
 
-  # Configure all nodes in nodeset
   c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => module_root, :module_name => module_name)
-
-    hosts.each do |host|
-      BeakerSpecHelper::spec_prep(host)
+    hosts.each do |h|
+      install_puppet_module_via_pmt_on(h, :module_name => 'puppetlabs-inifile')
+      puppet_pp = <<-EOF
+      ini_setting { 'puppet.conf/main/show_diff':
+        ensure  => 'present',
+        section => 'main',
+        path    => '/etc/puppet/puppet.conf',
+        setting => 'show_diff',
+        value   => 'true',
+      }
+      ini_setting { 'puppet.conf/main/parser':
+        ensure  => 'present',
+        section => 'main',
+        path    => '/etc/puppet/puppet.conf',
+        setting => 'parser',
+        value   => 'future',
+      }
+      EOF
+      apply_manifest_on(h, puppet_pp, :catch_failures => true)
     end
   end
 end
